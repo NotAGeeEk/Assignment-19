b882b634a1835b6423d4976b0a2ad8428ba861a2
Amans <notshashank10@gmail.com>
2023-07-31 15:32:25 +0530

    first commit

---
+++ ConsoleAppDelegates/ConsoleAppDelegates/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDelegates
+{
+    
+ 
+        public delegate int ArithmeticOperation(int a, int b);
+
+        class Program
+        {
+            static int Add(int a, int b)
+            {
+                return a + b;
+            }
+
+            static int Subtract(int a, int b)
+            {
+                return a - b;
+            }
+
+            static int Multiply(int a, int b)
+            {
+                return a * b;
+            }
+
+            static int Divide(int a, int b)
+            {
+                if (b != 0)
+                    return a / b;
+                else
+                {
+                    Console.WriteLine("Cannot divide by zero!");
+                    return 0;
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                ArithmeticOperation addOperation = new ArithmeticOperation(Add);
+                ArithmeticOperation subtractOperation = new ArithmeticOperation(Subtract);
+                ArithmeticOperation multiplyOperation = new ArithmeticOperation(Multiply);
+                ArithmeticOperation divideOperation = new ArithmeticOperation(Divide);
+
+                Console.WriteLine("Enter two integers:");
+                int num1 = int.Parse(Console.ReadLine());
+                int num2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Choose an arithmetic operation:");
+                Console.WriteLine("1. Addition");
+                Console.WriteLine("2. Subtraction");
+                Console.WriteLine("3. Multiplication");
+                Console.WriteLine("4. Division");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                int result = 0;
+                switch (choice)
+                {
+                    case 1:
+                        result = addOperation(num1, num2);
+                        break;
+                    case 2:
+                        result = subtractOperation(num1, num2);
+                        break;
+                    case 3:
+                        result = multiplyOperation(num1, num2);
+                        break;
+                    case 4:
+                        result = divideOperation(num1, num2);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice!");
+                        break;
+                }
+
+                Console.WriteLine($"Result: {result}");
+
+                Console.ReadKey();
+            }
+        }
+    }
+
---
+++ ConsoleAppGenericDelegates/ConsoleAppGenericDelegates/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment19_Part2
+{
+    // Create a generic delegate named Operation <T>
+    public delegate T Operation<T>(T num1, T num2);
+
+    internal class Program
+    {
+        //  Implement four static methods here : Add, Subtract, Multiply, and Divide
+        static T Add<T>(T num1, T num2)
+        {
+            dynamic a = num1;
+            dynamic b = num2;
+            return a + b;
+        }
+
+        static T Subtract<T>(T num1, T num2)
+        {
+            dynamic a = num1;
+            dynamic b = num2;
+            return a - b;
+        }
+
+        static T Multiply<T>(T num1, T num2)
+        {
+            dynamic a = num1;
+            dynamic b = num2;
+            return a * b;
+        }
+
+        static T Divide<T>(T num1, T num2)
+        {
+            dynamic a = num1;
+            dynamic b = num2;
+            if (b == 0)
+            {
+                throw new ArgumentException("Cannot divide by zero.");
+            }
+            return a / b;
+        }
+
+
+        static void Main(string[] args)
+        {
+            
+            Operation<double> addDelegate = Add;
+            Operation<double> subtractDelegate = Subtract;
+            Operation<double> multiplyDelegate = Multiply;
+            Operation<double> divideDelegate = Divide;
+
+         
+
+            Console.WriteLine("Enter two values of the same data type:");
+            string inputType = Console.ReadLine();
+            Console.WriteLine("Enter the first value:");
+            dynamic value1 = Convert.ChangeType(Console.ReadLine(), Type.GetType(inputType));
+            Console.WriteLine("Enter the second value:");
+            dynamic value2 = Convert.ChangeType(Console.ReadLine(), Type.GetType(inputType));
+
+           
+            Console.WriteLine("Choose an operation:");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+
+            int choice;
+            if (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 4)
+            {
+                Console.WriteLine("Invalid choice.");
+                return;
+            }
+
+            //Here it Call the corresponding method through the generic delegate based on user's choice
+            try
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine($"Result: {addDelegate(value1, value2)}");
+                        break;
+                    case 2:
+                        Console.WriteLine($"Result: {subtractDelegate(value1, value2)}");
+                        break;
+                    case 3:
+                        Console.WriteLine($"Result: {multiplyDelegate(value1, value2)}");
+                        break;
+                    case 4:
+                        Console.WriteLine($"Result: {divideDelegate(value1, value2)}");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error: Invalid input or operation.");
+            }
+        }
+    }
+}
